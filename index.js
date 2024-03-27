e0e7b204f61b37ae5bae209106e9edea2c548ffb
Mohit Reddy <114643178+M0hitReddy@users.noreply.github.com>
2024-03-28 01:03:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ import { createConnection } from "./connection.js";
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: true }));
 // appse(bodyPa).
-const url = 'https://qinjibrzhcbeniozyxyo.supabase.co';
-const key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpbmppYnJ6aGNiZW5pb3p5eHlvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTM2OTQyNCwiZXhwIjoyMDI2OTQ1NDI0fQ.RloB2tu8OMZwnE7ai9eehpQZFj3JTBrynTtF8-1U9Mw';
+const url = 'database_url';
+const key = 'database_key';
 createConnection(url, key)
 // app.get("/todos", (req, res) => {
 //     res.send("Hello World");
