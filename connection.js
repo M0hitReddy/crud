98e41d710b8af0c7d0c5e97b669d7b9b9c29a6c5
Mohit Reddy <114643178+M0hitReddy@users.noreply.github.com>
2024-03-28 01:02:18 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -3,9 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 let superbase;
 const createConnection = (url, key) => {
 
-    superbase = createClient(url, key
-        // 'https://qinjibrzhcbeniozyxyo.supabase.co',
-        // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpbmppYnJ6aGNiZW5pb3p5eHlvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTM2OTQyNCwiZXhwIjoyMDI2OTQ1NDI0fQ.RloB2tu8OMZwnE7ai9eehpQZFj3JTBrynTtF8-1U9Mw'
-    )
+    superbase = createClient(url, key);
 }
 export { superbase, createConnection }
